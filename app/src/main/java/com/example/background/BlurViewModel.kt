524fff4cfcdc29bd34ca6e1128bf2db67f2da9dc
이상현 <sanghyun.lee@deleo.co.kr>
2022-06-28 15:41:09 +0900

    WorkManager work chaining 추가함.

--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -28,29 +28,35 @@ import com.example.background.workers.CleanupWorker
 import com.example.background.workers.SaveImageToFileWorker
 
 
-class BlurViewModel(application: Application) : ViewModel() {
-    private var workerManager: WorkManager
-    internal var imageUri: Uri? = null
-    internal var outputUri: Uri? = null
+class BlurViewModel(application : Application) : ViewModel() {
+    private var workerManager : WorkManager
+    internal var imageUri : Uri? = null
+    internal var outputUri : Uri? = null
 
     init {
         imageUri = getImageUri(application.applicationContext)
         workerManager = WorkManager.getInstance(application)
     }
+
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
-    internal fun applyBlur(blurLevel: Int) {
-        val continuation = workerManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
-        val blurRequest = OneTimeWorkRequestBuilder<BluerWorker>().setInputData(
-            createInputDataForUri()
-        ).build()
+    internal fun applyBlur(blurLevel : Int) {
+        var continuation = workerManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
         val saveRequest = OneTimeWorkRequest.Builder(SaveImageToFileWorker::class.java).build()
-        continuation.then(blurRequest).then(saveRequest).enqueue()
+        for (i in 0 until blurLevel) {
+            val blurRequest = OneTimeWorkRequestBuilder<BluerWorker>()
+            if (i == 0) {
+                blurRequest.setInputData(createInputDataForUri())
+            }
+            continuation = continuation.then(blurRequest.build())
+        }
+
+        continuation.then(saveRequest).enqueue()
     }
 
-    private fun createInputDataForUri(): Data {
+    private fun createInputDataForUri() : Data {
         val builder = Data.Builder()
         imageUri?.let {
             builder.putString(KEY_IMAGE_URI, it.toString())
@@ -58,7 +64,7 @@ class BlurViewModel(application: Application) : ViewModel() {
         return builder.build()
     }
 
-    private fun uriOrNull(uriString: String?): Uri? {
+    private fun uriOrNull(uriString : String?) : Uri? {
         return if (!uriString.isNullOrEmpty()) {
             Uri.parse(uriString)
         } else {
@@ -66,7 +72,7 @@ class BlurViewModel(application: Application) : ViewModel() {
         }
     }
 
-    private fun getImageUri(context: Context): Uri {
+    private fun getImageUri(context : Context) : Uri {
         val resources = context.resources
 
         val imageUri = Uri.Builder()
@@ -79,13 +85,13 @@ class BlurViewModel(application: Application) : ViewModel() {
         return imageUri
     }
 
-    internal fun setOutputUri(outputImageUri: String?) {
+    internal fun setOutputUri(outputImageUri : String?) {
         outputUri = uriOrNull(outputImageUri)
     }
 
-    class BlurViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+    class BlurViewModelFactory(private val application : Application) : ViewModelProvider.Factory {
 
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        override fun <T : ViewModel?> create(modelClass : Class<T>) : T {
             return if (modelClass.isAssignableFrom(BlurViewModel::class.java)) {
                 BlurViewModel(application) as T
             } else {
